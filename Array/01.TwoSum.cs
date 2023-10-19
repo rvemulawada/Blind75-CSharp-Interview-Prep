2c784addc6cd907d71e565f8f6b7d965a56eaafa
Rakesh Vemulawada <82715387+rvemulawada@users.noreply.github.com>
2023-10-19 19:53:13 -0400

    Update 01.TwoSum.cs

--- Array/01.TwoSum.cs
+++ Array/01.TwoSum.cs
@@ -1,20 +1,3 @@
-## Problem Statement
-
-Given an array of integers `nums` and an integer `target`, your task is to find and return the indices of 
-two numbers in the array such that they add up to the given `target`. You can assume that each input will 
-have exactly one valid solution, and you should not use the same element twice in your answer. The order in
-which you return the indices does not matter.
-
-### Example 1
-#### Input
-
-nums = [2, 7, 11, 15]
-target = 9
-Output: [0, 1]
-Explanation
-Because nums[0] + nums[1] == 9, we return [0, 1].
-
-```csharp
 public int[] TwoSum(int[] nums, int target)
 {
     if (nums == null || nums.Length == 0)
