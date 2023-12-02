b3a9bb3f6dffb233de328ef54bdba6409dea9eb3
Rakesh <82715387+rvemulawada@users.noreply.github.com>
2023-12-02 14:44:00 -0500

    Added problem statement

--- LinkedList/16.ReverseALinkedList.cs
+++ LinkedList/16.ReverseALinkedList.cs
@@ -1,3 +1,11 @@
+Problem Statement:
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+Example 1:
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+-----------------------------------------------------------------------------
+
 public ListNode ReverseList(ListNode head) {
     ListNode prev = null;
     
